0f12f6ef18ed333317dd81082d230abdd414bb4f
Bart Thate <bthate@dds.nl>
2023-07-06 13:22:41 +0200

    add test

---
+++ tests/test_parse.py
@@ -0,0 +1,18 @@
+# This file is placed in the Public Domain.
+#
+# pylint: disable=C0114,C0115,C0116,W0703,C0413
+# pylama: ignore=E402
+
+
+import unittest
+
+
+from opr.parsers import parse
+
+
+class TestDecoder(unittest.TestCase):
+
+    def test_parse(self):
+        prs = parse("cmd")
+        print(prs)
+        self.assertEqual(prs["cmd"], "cmd")
