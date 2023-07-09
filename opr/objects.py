4fe03de1f10ec2c7c1b005f0f06e6d18b4357c74
Bart Thate <bthate@dds.nl>
2023-07-09 15:14:25 +0200

    move persist into object

--- opr/objects.py
+++ opr/objects.py
@@ -1,7 +1,10 @@
 # This file is placed in the Public Domain.
 
 
-""""clean namespace"""
+"""clean namespace"""
+
+
+__author__ = "Bart Thate <programmingobject@gmail.com>"
 
 
 # IMPORTS
@@ -10,7 +13,11 @@
 import datetime
 import json
 import os
+import pathlib
+import sys
+import time
 import uuid
+import _thread
 
 
 # INTERFACE
@@ -18,39 +25,48 @@ import uuid
 
 def __dir__():
     return (
+            'Persist',
             'Object',
-            'ObjectDecoder',
-            'ObjectEncoder',
             'copy',
-            'dump',
-            'dumps',
             'edit',
+            'find',
             'ident',
             'items',
             'keys',
             'kind',
-            'load',
-            'loads',
+            'last',
             'prt',
+            'read',
+            'readrec',
+            'search',
             'update',
-            'values'
+            'values',
+            'write',
+            'writerec'
            )
 
 
 __all__ = __dir__()
 
 
-# CLASSES
+# DEFINES
+
+
+disklock = _thread.allocate_lock()
+
+
+# OBJECT
 
 
 class Object:
 
-    """clean namespace"""
+    """doesn't have any methods, just dunders"""
 
-    __slots__ = ('__dict__', '__oid__')
+    __slots__ = ('__default__', '__dict__', '__oid__')
 
     def __init__(self):
         """construct an object without any methods, just dunders"""
+        self.__default__ = ""
         self.__oid__ = ident(self)
 
     def __contains__(self, key):
@@ -60,6 +76,10 @@ class Object:
     def __delitem__(self, key):
         """remove key from object"""
         return self.__dict__.__delitem__(key)
+
+    def __getattr__(self, key):
+        ""
+        return self.__dict__.get(key, self.__default__)
 
     def __getitem__(self, key):
         """dict like access"""
@@ -80,28 +100,6 @@ class Object:
     def __str__(self):
         """return recursive json string"""
         return dumps(dumprec(self))
-
-
-class Default(Object):
-
-    """default return values"""
-
-    __slots__ = ("__default__",)
-
-    def __init__(self, *args, **kwargs):
-        Object.__init__(self, *args, **kwargs)
-        self.__default__ = ""
-
-    def __getattr__(self, key):
-        ""
-        return self.__dict__.get(key, self.__default__)
-
-    def __setdefault__(self, default):
-        ""
-        self.__default__ = default
-
-
-# METHODS
 
 
 def copy(obj, val) -> None:
@@ -235,6 +233,22 @@ def prt(obj, args="", skip="", plain=False):
     return txt.strip()
 
 
+def search(obj, selector) -> bool:
+    """check whether values in selector dict match in the object"""
+    res = False
+    select = Object()
+    copy(select, selector)
+    for key, value in items(select):
+        try:
+            val = getattr(obj, key)
+        except AttributeError:
+            continue
+        if str(value) in str(val):
+            res = True
+            break
+    return res
+
+
 def update(obj, data, empty=True) -> None:
     """update an object with a data dictionary"""
     for key, value in items(data):
@@ -337,3 +351,205 @@ def dumps(*args, **kw) -> str:
     """json dump to string with object encoder"""
     kw["cls"] = ObjectEncoder
     return json.dumps(*args, **kw)
+
+
+# PERSIST
+
+
+class Persist:
+
+    """directory to persist to"""
+
+    workdir = ""
+
+    @staticmethod
+    def path(pth) -> str:
+        """return store path"""
+        return os.path.join(Persist.workdir, 'store', pth)
+
+    @staticmethod
+    def storedir() -> str:
+        """return storage directory"""
+        return os.path.join(Persist.workdir, "store")
+
+
+def cdir(pth) -> None:
+    "Create directory"
+    if not pth.endswith(os.sep):
+        pth = os.path.dirname(pth)
+    pth = pathlib.Path(pth)
+    os.makedirs(pth, exist_ok=True)
+
+
+def files() -> []:
+    """show all files in store"""
+    res = []
+    path = Persist.storedir()
+    if os.path.exists(path):
+        res = os.listdir(path)
+    return res
+
+
+def find(mtc, selector=None) -> []:
+    """locate specific objects"""
+    if selector is None:
+        selector = {}
+    for fnm in fns(mtc):
+        obj = hook(fnm)
+        if '__deleted__' in obj:
+            continue
+        if selector and not search(obj, selector):
+            continue
+        yield obj
+
+
+def fnclass(pth) -> str:
+    """return class from filename"""
+    try:
+        *_rest, mpth = pth.split("store")
+        splitted = mpth.split(os.sep)
+        return splitted[0]
+    except ValueError:
+        pass
+    return None
+
+
+def fns(mtc) -> []:
+    """return matching filenames"""
+    dname = ''
+    lst = mtc.lower().split(".")[-1]
+    for rootdir, dirs, _files in os.walk(Persist.storedir(), topdown=False):
+        if dirs:
+            dname = sorted(dirs)[-1]
+            if dname.count('-') == 2:
+                ddd = os.path.join(rootdir, dname)
+                fls = sorted(os.listdir(ddd))
+                if fls:
+                    path2 = os.path.join(ddd, fls[-1])
+                    splitted = strip(path2).split(os.sep, maxsplit=1)[0]
+                    if lst in splitted.lower().split(".")[-1]:
+                        yield strip(path2)
+
+
+def fntime(daystr) -> float:
+    """return time from filename"""
+    daystr = daystr.replace('_', ':')
+    datestr = ' '.join(daystr.split(os.sep)[-2:])
+    if '.' in datestr:
+        datestr, rest = datestr.rsplit('.', 1)
+    else:
+        rest = ''
+    tme = time.mktime(time.strptime(datestr, '%Y-%m-%d %H:%M:%S'))
+    if rest:
+        tme += float('.' + rest)
+    else:
+        tme = 0
+    return tme
+
+
+def hook(otp) -> type:
+    """return object from filename"""
+    clz = fnclass(otp)
+    splitted = clz.split(".")
+    modname = ".".join(splitted[:1])
+    clz = splitted[-1]
+    mod = sys.modules.get(modname, None)
+    if mod:
+        cls = getattr(mod, clz, None)
+    if cls:
+        obj = cls()
+        read(obj, otp)
+        return obj
+    obj = Object()
+    read(obj, otp)
+    return obj
+
+
+def spl(txt) -> []:
+    """split comma seperated string"""
+    try:
+        res = txt.split(',')
+    except (TypeError, ValueError):
+        res = txt
+    return [x for x in res if x]
+
+
+def strip(pth) -> str:
+    "strip path to ident part"
+    return os.sep.join(pth.split(os.sep)[-4:])
+
+
+def touch(fname) -> None:
+    "touch a file"
+    fds = os.open(fname, os.O_WRONLY | os.O_CREAT)
+    os.close(fds)
+
+
+## METHODS
+
+
+def last(obj, selector=None) -> None:
+    """update with last saved version"""
+    if selector is None:
+        selector = {}
+    result = sorted(
+                    find(kind(obj), selector),
+                    key=lambda x: fntime(x.__oid__)
+                   )
+    if result:
+        inp = result[-1]
+        update(obj, inp)
+        obj.__oid__ = inp.__oid__
+    return obj.__oid__
+
+
+def read(obj, pth) -> str:
+    """read object from path"""
+    pth = Persist.path(pth)
+    with disklock:
+        with open(pth, 'r', encoding='utf-8') as ofile:
+            data = load(ofile)
+            update(obj, data)
+    obj.__oid__ = strip(pth)
+    return obj.__oid__
+
+
+def readrec(obj, pth=None) -> type:
+    """read object recursively"""
+    ooo = type(obj)()
+    if pth:
+        read(ooo, pth)
+    else:
+        update(ooo, obj)
+    oooo = type(obj)()
+    for key, value in items(ooo):
+        if issubclass(type(value), Object):
+            setattr(oooo, key, readrec(value))
+            continue
+        setattr(oooo, key, value)
+    return oooo
+
+
+def write(obj) -> str:
+    """write object to disk"""
+    try:
+        pth = obj.__oid__
+    except TypeError:
+        pth = ident(obj)
+    pth = Persist.path(pth)
+    cdir(pth)
+    with disklock:
+        with open(pth, 'w', encoding='utf-8') as ofile:
+            dump(obj, ofile)
+    return strip(pth)
+
+
+def writerec(obj):
+    """write object recursively"""
+    ooo = type(obj)()
+    for key, value in items(obj):
+        if issubclass(type(value), Object):
+            setattr(ooo, key, writerec(value))
+        else:
+            setattr(ooo, key, str(value))
+    return write(ooo)
