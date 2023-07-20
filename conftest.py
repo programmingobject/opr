f0211be64fcf0b34913eeebf3e610f1f0aca2c55
Bart Thate <programmingobject@gmail.com>
2023-07-20 02:11:01 +0200

    use exit status 0

--- conftest.py
+++ conftest.py
@@ -9,5 +9,5 @@ __author__ = "Bart Thate <programmingobject@gmail.com>"
 
 def pytest_sessionfinish(session, exitstatus):
     if exitstatus == 5:
-        session.exitstatus = 10 # Any arbitrary custom status you want to return```
+        session.exitstatus = 0 # Any arbitrary custom status you want to return```
         
